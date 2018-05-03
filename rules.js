7299d892e98e72ca3788722129a2edb38965dd6f
Bill Butler <bill@butler.net>
2018-05-03 18:51:39 -0500

    add rules array

---
+++ rules.js
@@ -0,0 +1,18 @@
+var rules = [
+    {
+        baseSymbol: 'GDEX.EOS',
+        sellSymbol: 'OPEN.EOS',
+        qty: 50,
+        center: 1,
+        target: 0.008
+    },  
+    {
+        baseSymbol: 'OPEN.LTC',
+        sellSymbol: 'BRIDGE.LTC',
+        qty: 8,
+        center: 1,
+        target: 0.008
+    }
+]
+
+module.exports = rules;
